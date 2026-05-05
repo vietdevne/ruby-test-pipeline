1fc51aabe405a4fdf02fd640a8c5643a7a290bdd
vietnq1-nals <vietnq1@mynavitechtus.com>
2026-05-05 09:23:52 +0700

    update build.sh

--- uploadDir/build.sh
+++ uploadDir/build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 echo "Hello anh em hehehehe"
-../bin/setup
+source ~/.bashrc
+
+export RBENV_ROOT="/home/ubuntu/.rbenv"
+export PATH="$RBENV_ROOT/bin:$RBENV_ROOT/shims:$PATH"
+eval "$(rbenv init - bash)"
+rbenv global 4.0.3
+
+bundle install
+rails db:migrate
